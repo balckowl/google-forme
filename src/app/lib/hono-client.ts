03b05785556f3aa436d0ecb0063ee932c923f89f
balckowl <shiggyjr1492@gmail.com>
2025-11-09 21:39:56 +0900

    fix: version

--- src/app/lib/hono-client.ts
+++ src/app/lib/hono-client.ts
@@ -1,6 +1,7 @@
 import { hc } from "hono/client";
+import { env } from "@/env.mjs";
 import type { pub } from "@/server/hono";
 
 type AppType = typeof pub;
 
-export const hono = hc<AppType>("https://goodle-forme.vercel.app");
+export const hono = hc<AppType>(env.NEXT_PUBLIC_APP_URL);
