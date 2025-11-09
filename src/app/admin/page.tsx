e451085f9890116801ea057c458781f865fb2eca
balckowl <shiggyjr1492@gmail.com>
2025-11-09 22:48:25 +0900

    fix

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,16 @@
-import { db } from "@/db";
 import AdminWrapper from "../components/AdminWrapper";
+import { hono } from "../lib/hono-client";
+
+export const dynamic = 'force-dynamic'
 
 export default async function Page() {
-  const allFlags = await db.query.featureFlags.findMany();
-  return <AdminWrapper flags={allFlags[0]} />;
+  const res = await hono.api.allflags.$get();
+
+  if (res.status !== 200) {
+    throw Error("何かの問題が発生しました。")
+  }
+
+  const flags = await res.json();
+
+  return <AdminWrapper flags={flags} />;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-import { db } from "@/db";
 import FormWrapper from "./components/FormWrapper";
+import { hono } from "./lib/hono-client";
+
+export const dynamic = 'force-dynamic'
 
 export default async function Page() {
-  const allFlags = await db.query.featureFlags.findMany();
-  return <FormWrapper flags={allFlags[0]} />;
+  const res = await hono.api.allflags.$get();
+
+  if (res.status !== 200) {
+    throw Error("何かの問題が発生しました。")
+  }
+
+  const flags = await res.json();
+
+  return <FormWrapper flags={flags} />;
 }
