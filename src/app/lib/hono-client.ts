c9973cace3e358513e061079208ae51ac5d4933f
balckowl <shiggyjr1492@gmail.com>
2025-11-09 22:32:22 +0900

    fix

--- src/app/lib/hono-client.ts
+++ src/app/lib/hono-client.ts
@@ -1,6 +1,7 @@
 import { hc } from "hono/client";
+import { env } from "@/env.mjs";
 import type { pub } from "@/server/hono";
 
 type AppType = typeof pub;
 
-export const hono = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL as string);
+export const hono = hc<AppType>(env.NEXT_PUBLIC_APP_URL);
